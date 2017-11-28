2f18cc9dbb01d0ad38b46dc69f4937593389dca2
luzhizhuang <php_xiaolu@sina.com>
2017-11-28 10:28:58 +0800

    提取公共文件

---
+++ app/Http/Controllers/Admin/AdController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+         echo '111';
+    }
+
+    /**
+     * 广告添加模块
+     *
+     * 
+     * @return 返回一个添加模板
+     */
+    public function create()
+    {
+        // $id = $_GET['id']; 哪个用户添加的广告
+        $id = 1;
+        return view('Admin.Ad.create',['title'=>'广告添加','uid'=>$id]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+         echo '111';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/Admin/Ad/create.blade.php
@@ -0,0 +1,161 @@
+@extends('Admin.layout')
+
+
+@section('content')
+
+<div class="tpl-content-wrapper">
+
+            <div class="container-fluid am-cf">
+                <div class="row">
+                    <div class="am-u-sm-12 am-u-md-12 am-u-lg-9">
+                        <div class="page-header-heading"><span class="am-icon-home page-header-heading-icon"></span> 表单 <small>Amaze UI</small></div>
+                        <p class="page-header-description">Amaze UI 有许多不同的表格可用。</p>
+                    </div>
+                    <div class="am-u-lg-3 tpl-index-settings-button">
+                        <button class="page-header-button" type="button"><span class="am-icon-paint-brush"></span> 设置</button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row-content am-cf">
+
+
+               
+                
+                <div class="row">
+
+                    <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
+                        <div class="widget am-cf">
+                           
+                            <div class="widget-body am-fr">
+
+                                <form action="" method="post" enctype="" class="am-form tpl-form-border-form">
+
+								<div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label am-text-left" for="user-name">添加者<span class="tpl-form-line-small-title"></span></label>
+                                        <div class="am-u-sm-12">
+                                            <input type="text" readonly=""   name="adesc" id="user-name" value="{{$uid}}" class="tpl-form-input am-margin-top-xs">
+                                            
+                                        </div>
+                                    </div>
+
+                                    <div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label am-text-left" for="user-name">请对广告做出描述 <span class="tpl-form-line-small-title">dsec</span></label>
+                                        <div class="am-u-sm-12">
+                                            <input type="text" placeholder="广告描述" name="adesc" id="user-name" class="tpl-form-input am-margin-top-xs">
+                                           
+                                        </div>
+                                    </div>
+                                       <div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label am-text-left" for="user-name">请输入广告链接 <span class="tpl-form-line-small-title">link</span></label>
+                                        <div class="am-u-sm-12">
+                                            <input type="text" placeholder="链接地址" name="acontent" id="user-name" class="tpl-form-input am-margin-top-xs">
+                                        
+                                        </div>
+                                    </div>
+
+                                    <div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label am-text-left" for="user-email">开始时间 <span class="tpl-form-line-small-title">startTime</span></label>
+                                        <div class="am-u-sm-12">
+                                            <input type="text" id="startTime" name="startTime" readonly="" data-am-datepicker="" placeholder="点击输入框填入" class="am-form-field tpl-form-no-bg am-margin-top-xs">
+                                        </div>
+                                    </div>
+                                    <div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label am-text-left" for="user-email">结束时间 <span class="tpl-form-line-small-title">endTime</span></label>
+                                        <div class="am-u-sm-12">
+                                            <input type="text" id="endTime" name="endTime" readonly="" data-am-datepicker="" placeholder="点击输入框填入" class="am-form-field tpl-form-no-bg am-margin-top-xs">
+                                        </div>
+                                    </div>
+
+
+
+                                    
+                                    <div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label  am-text-left" for="user-weibo">请上传广告图片 <span class="tpl-form-line-small-title">Images</span></label>
+                                        <div class="am-u-sm-12 am-margin-top-xs">
+                                            <div class="am-form-group am-form-file">
+                                                <div class="tpl-form-file-img">
+                                                    <img alt="" src="assets/img/a5.png">
+                                                </div>
+                                                <button style="button" class="am-btn am-btn-danger am-btn-sm " type="button">
+    <i class="am-icon-cloud-upload"></i> 请上传广告图片</button>
+                                                <input type="file" name="aimg" multiple="" id="doc-form-file">
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="am-form-group">
+                                        <label class="am-u-sm-12 am-form-label  am-text-left" for="user-weibo">共计金额 <span class="tpl-form-line-small-title"></span></label>
+                                        <div class="am-u-sm-12">
+                                            <input type="text" id="jine" name="aprice" placeholder="" class="am-margin-top-xs" id="user-weibo">
+                                            <div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    
+
+                                   
+
+                                    <div class="am-form-group">
+                                        <div class="am-u-sm-12 am-u-sm-push-12">
+                                            <button class="am-btn am-btn-primary tpl-btn-bg-color-success " type="button">提交</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+<script type="text/javascript">
+	//点击结束时间获取开始时间的值
+	$('#jine').on('click',function()
+		{
+			//获取开始时间的时间戳
+			var startTime = $('#startTime').val();
+			var timestamp = Date.parse(new Date(startTime));
+			startstamp = timestamp / 1000;
+			//获取结束时间的时间戳
+			var endTime = $('#endTime').val();
+			var timestamp = Date.parse(new Date(endTime));
+			endstamp = timestamp / 1000;
+
+			//判断时间是不是没选
+			if(!startstamp){
+				alert('请输入开始时间');
+			}
+			if(!endstamp){
+				alert('请输入结束时间');
+			}
+
+			//计算几天  多少钱
+			//天数*10
+			if(endstamp > startstamp){
+
+				var time = endstamp - startstamp;
+				time = Math.ceil(time/(60*60*24));
+				var jine = time * 10;
+
+				$('#jine').val(jine);
+
+			}
+			
+
+
+
+
+		});
+
+</script>
+
+
+
+@stop
